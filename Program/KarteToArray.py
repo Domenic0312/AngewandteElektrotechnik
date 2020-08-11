a3c6a684a4ebd395db22fd98e1a834d172b731ac
Domenic Drechsel <43642192+Domenic0312@users.noreply.github.com>
2020-08-11 11:37:53 +0200

    kartenZeugs

--- Program/KarteToArray.py
+++ Program/KarteToArray.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd 
 
+Schrittweite = 5
+
 my_path = os.path.dirname(__file__)
 imagePath = my_path + "/Karte_RAW.png"
 
@@ -11,21 +13,23 @@ np.set_printoptions(precision=3)
 im = Image.open(imagePath)
 rgb_im = im.convert('RGB')
 width, height = rgb_im.size
+width = int(width/Schrittweite)
+height = int(height/Schrittweite)
 
 print(width, height)
 arr = np.zeros((width, height)) 
 
 for i in range(0, width):
     for j in range(0, height):
-        r, g, b = rgb_im.getpixel((i, j))
+        r, g, b = rgb_im.getpixel((i*5, j*5))
         if g>250 and r<50 and b<50:
             arr[i,j] = 1
-            print("habe was", i,j)
+            #print("habe was", i,j)
         else:
             arr[i,j] = 0
 
 arr = np.around(arr, decimals=0)
 print(arr)
-np.savetxt((my_path+"/karde.csv"), arr, delimiter=';', fmt='% 4d')
+np.savetxt((my_path+"/karteSmall.csv"), arr, delimiter=';', fmt='% 4d')
 
 print("Wenn Sie dies hier lesen koennen, dann ist das Programm erfolgreich durchgelaufen. Einen schoenen Tag wuenscht das Team von DomSoft")
