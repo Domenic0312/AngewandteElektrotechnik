bb0df7acfd5b797df3f73c2e04b914dec9c28627
Domenic Drechsel <43642192+Domenic0312@users.noreply.github.com>
2020-08-12 09:45:30 +0200

    dsa

--- Program/cSharp/Sensoren.cs
+++ Program/cSharp/Sensoren.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using KartenDaten;
 
 namespace Sensoren
 {
@@ -26,5 +27,11 @@ namespace Sensoren
     }
     class distanceSensors:sensor{
 
+        public distanceSensors(String _Name, int _pinOne, int _pinTwo){
+          Name = _Name;
+        }
+        void getDistance(){
+            
+        }
     }
 }
