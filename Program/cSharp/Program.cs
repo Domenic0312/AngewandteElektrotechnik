e3d26f14a96edcb3c8df697868472f594f1245a9
Domenic0312 <43642192+Domenic0312@users.noreply.github.com>
2020-08-13 09:19:30 +0200

    Änderungen

--- Program/cSharp/Program.cs
+++ Program/cSharp/Program.cs
@@ -11,10 +11,18 @@ namespace cSharp
             //Karte generieren (Karten array fuellen)
             Karte.genKarte();
 
+            List<distanceSensor> sensorik = new List<distanceSensor>();
+
+            for(int i=0;i<8;i++){
+                sensorik.append(new distanceSensor("Name...",0 ,1 , i));
+            }
+
+
             distanceSensor distOne = new distanceSensor("SensorgenNorden",0,1,0);
 
-            //Abstand von der Position 44/65 nach Norden abfragen
-            int distance = distOne.getDistance(66,164);
+            //Abstand von der Position  nach Norden abfragen
+            int distance = distOne.getDistance(100,160);
+
             Console.WriteLine("Abstand ist: {0}",distance);
         }
     }
