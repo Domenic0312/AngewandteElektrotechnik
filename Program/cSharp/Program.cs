e4f84257dbd756b2297fa19f5c65ecebdd1a139f
gitkatrin <43642275+gitkatrin@users.noreply.github.com>
2020-08-15 16:58:01 +0200

    optimize robot

--- Program/cSharp/Program.cs
+++ Program/cSharp/Program.cs
@@ -2,6 +2,7 @@
 using KartenDaten;
 using Sensoren;
 using System.Collections.Generic;
+using RescueRobot;
 
 namespace cSharp
 {
@@ -12,12 +13,20 @@ namespace cSharp
             //Karte generieren (Karten array fuellen)
             Karte.genKarte();
 
-            distanceSensor distOne = new distanceSensor("SensorgenNorden",0,1,0);
+            distanceSensor distOne = new distanceSensor("SensorgenNorden", 0, 1, 0);
 
             //Abstand von der Position  nach Norden abfragen
-            int distance = distOne.getDistance(100,160);
+            int distance = distOne.getDistance(100, 160);
 
-            Console.WriteLine("Abstand ist: {0}",distance);
+            Console.WriteLine("Abstand ist: {0}", distance);
+
+            Robot robot = new Robot();
+            robot.distanceSensoren();
+            robot.drive();
+
+            PowerTrain power = new PowerTrain();
+            power.getBatteryState();
+
         }
     }
 }
