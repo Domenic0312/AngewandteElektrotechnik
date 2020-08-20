84dae7c0ee9d016494a20ecedde4ba412d032aeb
Domenic0312 <43642192+Domenic0312@users.noreply.github.com>
2020-08-20 12:07:04 +0200

    BackupHochladen

---
+++ Program/cSharp/Sensoren.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using KartenDaten;
+
+namespace Sensoren
+{
+    class sensor{
+        public int[] Pins;
+        public String Name;
+
+        public void connect(){
+            Console.WriteLine("Verbindung zu Sensor {0} wurde aufgebaut. Pins: {1},{2}", Name, Pins[0], Pins[1]);
+        }
+        public void disconnect(){
+            Console.WriteLine("Verbindung zu Sensor {0} wurde beendet. Pins: {1},{2}", Name, Pins[0], Pins[1]);
+        }
+    }
+    class radioSensor:sensor{
+        public radioSensor(String _Name, int _pinOne, int _pinTwo){
+            Name = _Name;
+            Pins = new int[]{_pinOne, _pinTwo};
+            connect();
+        }
+    }
+    class waterSensor:sensor{
+        public waterSensor(String _Name, int _pinOne, int _pinTwo){
+            Name = _Name;
+            Pins = new int[]{_pinOne, _pinTwo};
+            connect();
+        }
+
+        public bool isWater(int posX, int posY, int viewDirection){
+            int terrain = Karte.getTerrain(posX, posY, viewDirection);
+            if(terrain == 2){
+                return true;
+            }
+            return false;
+        }
+
+    }
+    class battery:sensor{
+        int wert = 75;
+
+        public battery(String _Name, int pinOne, int pinTwo){
+            Name = _Name;
+            Pins = new int[]{pinOne, pinTwo};
+            connect();
+        }
+        public int getState(){
+            return wert;
+        }
+    }
+    class distanceSensor:sensor{
+        public int viewDirection = 0;
+        public distanceSensor(String _Name, int _pinOne, int _pinTwo, int _viewDirection){
+            Name = _Name;
+            Pins = new int[]{_pinOne,_pinTwo};
+            viewDirection = _viewDirection;
+            connect();
+        }
+
+        public distanceSensor(){}
+        public int getDistance(int posX, int posY){
+            int dist = Karte.getDistance(posX, posY, viewDirection);
+            Console.WriteLine("Abstand von Position {0}/{1} in Richtung {2} ist {3}",posX, posY, viewDirection, dist);
+            return dist;
+        }
+    }
+}
